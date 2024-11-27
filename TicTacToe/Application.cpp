a46b5bb76677dcc0d231154c1d9d3b53815835cf
McSimJr <mcsimfr@gmail.com>
2024-11-27 10:50:49 +0100

    Ajout Class Pion

--- TicTacToe/Application.cpp
+++ TicTacToe/Application.cpp
@@ -4,6 +4,34 @@
  * @brief Fichier source de l'application
  *********************************************************************/
 #include "Application.h"
+#define Circle 1
+#define Cross 2;
+#define Empty 0;
+
+class Pion{
+ int type;
+ public:
+ void SetType(int TypeIn){
+  type=TypeIn;
+ }
+
+int GetType(){
+  return type;
+}
+
+char GetChar(){
+  char Resultat;
+  if (type==Circle){
+    Resultat='o';
+  } else if (type==Cross) {
+    Resultat='x';
+  } else {
+    Resultat=' '; 
+  }
+  return Resultat;
+}
+Pion(int TypeIn);
+};
 
 
 Application::Application()
